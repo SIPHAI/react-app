091d2075e2e10b910f394fa93c5d4a1d61edf97a
SIPHAI <phalsiphai18@gmail.com>
2023-06-07 10:32:26 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { Component } from "react"
 import Message from "./Message"
 function app() {
-    return (
-        <div>
-            <h1>Hello What's up?</h1>
-        </div>
-    )
+  const name = 'Siphai';
+  if (name)
+     return <h1>Hello, it's {name}</h1>;
+  return <h1>Hello What's Up?</h1>;
 }
 export default app;
